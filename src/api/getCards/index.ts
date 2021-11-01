2885a7d3936b240ade7542560342f65fe052877f
Daniil <ventsovdaniil@yandex.ru>
2021-11-01 16:40:49 +0300

    fix

--- src/api/getCards/index.ts
+++ src/api/getCards/index.ts
@@ -2,6 +2,8 @@ import axios, { AxiosResponse } from "axios";
 
 export default class cardsApi {
   public static async getCards(): Promise<AxiosResponse<string[]>> {
-    return axios.get("/cards");
+    return axios.get("/cards", {
+      baseURL: "http://localhost:3000",
+    });
   }
 }
--- src/components/CardsGallery/index.ts
+++ src/components/CardsGallery/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import cardsApi from "@/api/getCards/index";
 
-console.log(cardsApi);
+console.log(cardsApi.getCards());
 
 export default class CardsGallery extends Vue {}
